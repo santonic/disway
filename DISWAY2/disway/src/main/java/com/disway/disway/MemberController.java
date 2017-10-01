bd1ac1fcb379ec42c24db4226701e4d1eb512a0a
santonic <root@santonic-S551LN>
2017-10-01 15:45:25 +0200

    join button adds random points

--- DISWAY2/disway/src/main/java/com/disway/disway/MemberController.java
+++ DISWAY2/disway/src/main/java/com/disway/disway/MemberController.java
@@ -15,13 +15,17 @@ public class MemberController {
 
 	AtomicInteger nextId = new AtomicInteger();
 	private Collection<Member> list;
+	
+	double DEFAULT_X = 265425.3;
+	double DEFAULT_Y = 6248108;
 
 	public MemberController() {
 		list = new HashSet<Member>();
-		list.add(new Member(nextId.getAndIncrement(), 265425.3, 6248108, "j'ai faim"));
-		list.add(new Member(nextId.getAndIncrement(), 265425.3 + 45, 6248108 + 60, "j'ai froid"));
-		list.add(new Member(nextId.getAndIncrement(), 265425.3 - 60, 6248108 + 90, "j'ai mal aux dents"));
-		list.add(new Member(nextId.getAndIncrement(), 265425.3 + 40, 6248108 - 80, "j'ai froid"));
+
+		list.add(new Member(nextId.getAndIncrement(), DEFAULT_X, DEFAULT_Y, "j'ai faim"));
+		list.add(new Member(nextId.getAndIncrement(), DEFAULT_X + 45, DEFAULT_Y + 60, "j'ai froid"));
+		list.add(new Member(nextId.getAndIncrement(), DEFAULT_X - 60, DEFAULT_Y + 90, "j'ai mal aux dents"));
+		list.add(new Member(nextId.getAndIncrement(), DEFAULT_X + 40, DEFAULT_Y - 80, "j'ai froid"));
 	}
 
 	@RequestMapping("/")
@@ -34,14 +38,33 @@ public class MemberController {
 		return list;
 	}
 
+	@RequestMapping("/members/_clear")
+	public void clearMembers() {
+		list.clear();
+	}
+	
 	@RequestMapping("/members/_join")
-	public Member addMember(@RequestParam double x, @RequestParam double y) {
+	public Member addMember(
+			@RequestParam(required=false) Double x, 
+			@RequestParam(required=false) Double y) {
 		int id = nextId.getAndIncrement();
+		if(x == null) {
+			x = randomize(DEFAULT_X);
+			y = randomize(DEFAULT_Y);
+		}
 		Member member = new Member(id, x, y, null);
 		list.add(member);
 		return member;
 	}
 	
+	private double randomize(double z) {
+		if(Math.random()<0.5) {
+			return z + Math.random()* 90;
+		} else {
+			return z - Math.random()* 90;
+		}
+	}
+
 	@RequestMapping("/members/_addProblem")
 	public Member addMember(@RequestParam long id, @RequestParam String problem) {
 		for (Member member : list) {
